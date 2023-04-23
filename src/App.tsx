e2f9bdf09d801e6c5a17ac7f8a065466830b6646
jayad23 <jaidervanegas@live.com>
2023-04-23 03:22:37 +0200

    integrating Material Ui list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import {Box, Typography} from '@mui/material';
-import React, { useState } from 'react'
+import React from 'react'
+import {Box, Typography, ListSubheader, List} from '@mui/material';
+import ProgramList from './components/program-list/ProgramList';
+
 interface inforationProps {
   id: number;
   label: string;
   topics: Array<string>;
 };
+
 function App() {
   const information: Array<inforationProps> = [
     { id: 1, label: "primera_sesion", topics: ["Instalación de MUi", "Componentes Básicos", "Paso de props a componentes", "Atributos bñasicos"]},
-    //{ id: 2, label: "segunda_sesion", topics: ["Etiquetas contenedoras y estilos default", "Texfield", "Grid System", "Combianos eso creando un Login y una vista Home en la que también vimos:", "Firebase", "React router Layout", "API Calls", "Split coding"]},
+    { id: 2, label: "segunda_sesion", topics: ["Etiquetas contenedoras y estilos default", "Texfield", "Grid System", "Combianos eso creando un Login y una vista Home en la que también vimos:", "Firebase", "React router Layout", "API Calls", "Split coding"]},
   ]
 
   return (
@@ -29,27 +32,22 @@ function App() {
       <Typography variant="h4" component="div" mb={2}>
         Bienvenidos a React con MUi & TypeScript
       </Typography>
-      <Typography variant="body1" component="p" mb={1}>
-        Para acceder a la información de las sesiones, debes dirigirte a las ramas/(branches):
-      </Typography>
-      <Box component="section" sx={{ paddingLeft: "20px"}}>
-        <ul>
-          {
+      <List
+        sx={{ width: '100%', bgcolor: 'background.paper' }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            Para acceder a la información de las sesiones, debes dirigirte a las ramas/(branches):
+          </ListSubheader>
+        }
+      >
+        {
             information.map((info: inforationProps) => (
-              <li key={info.id}>
-                {info.label}
-                <Box component="ul" sx={{ paddingLeft: "20px"}}>
-                  {
-                    info.topics.map((topic: string) => (
-                      <li key={topic}>{topic}</li>
-                    ))
-                  }
-                </Box>
-              </li>
+              <ProgramList key={info.id} {...info}/>
             ))
           }
-        </ul>
-      </Box>
+      </List>
     </Box>
   )
 }
