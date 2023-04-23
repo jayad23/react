2568458e37ea39260e295a6b78822b3c4ea62976
jayad23 <jaidervanegas@live.com>
2023-04-24 01:03:41 +0200

    added in react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import App from './App'
 import './index.css'
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
