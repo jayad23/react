079bf18c1d9ba6fb61eb5c80251c3be293114f4c
jayad23 <jaidervanegas@live.com>
2023-04-27 09:38:39 +0200

    moved in folder generic-types

---
+++ src/generic-types/types.ts
@@ -0,0 +1,10 @@
+// string // number // null // undefined 
+// Array [] // Record {}
+// any // unknown 
+//<T>
+
+export const getNames = (name: string): string => {
+  return name;
+};
+
+export interface ComponentsProps { nombre: string; apellido: string; edad: number }
