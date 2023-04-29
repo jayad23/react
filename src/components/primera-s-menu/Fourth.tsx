f00243cc1fe321570e7889d0eb52185c3607605b
jayad23 <jaidervanegas@live.com>
2023-04-29 20:05:05 +0200

    fix

--- src/components/primera-s-menu/Fourth.tsx
+++ src/components/primera-s-menu/Fourth.tsx
@@ -5,4 +5,4 @@ import { secondaryInfo } from '@assets/info';
 const subHeader = "En Material Ui hay dos tipos de compoentes. Los conoceremos como Componentes Simples, y componentes compuestos. Se llaman SIMPLES cuando el componente se puede utilizar directamente cómo ha sido importado. Se llaman COMPUESTOS cuando el componente requiere de otros elementos (no funciones) para poder ser utilizado. Independiente del tipo de componente, estos vienen estilos por default que pueden ser modificados por medio de atributos. En el siguiente punto de menú veremos algunos ejemplos.";
 
 
-export const Fourth = () => <Generic information={secondaryInfo.four} subHeader={subHeader}/>
+export const Fourth = () => <Generic information={[]} subHeader={subHeader}/>
